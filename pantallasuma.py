31c2c568f3a7aacd45766b73530c93bfd880c166
camipaujess <49650114+camipaujess@users.noreply.github.com>
2019-04-16 16:59:10 -0500

    Add files via upload

--- pantallasuma.py
+++ pantallasuma.py
@@ -63,40 +63,43 @@ def pantallaSuma():
         pygame.draw.polygon(pantalla, morado, [[500, 360],[ 475, 400], [525, 400]], 0)
 
         #parde de la suma
-        puntuacion = 0
-        rng = random.Random()
-        listapuntuacion = [50, 100, 150, 200]
-        rango = [10, 50, 100, 500]
-        for puntos in listapuntuacion:
-            while puntuacion < puntos:
-                c = rango[0]
-                a = rng.randrange(1,c)
-                b = rng.randrange(1,c)
-                res = a+b
-                res1 = int(input("¿Cuánto es " + str(a) + " + " + str(b) + "?  "))
-                if res1 == res:
-                    texto = fuente.render("Respuesta Correcta", True, negro)
-                    pantalla.blit(texto, (350, 250))
+def suma():
+    puntuacion = 0
+    rng = random.Random()
+    listapuntuacion = [50, 100, 150, 200, 250]
+    rango = [10, 30, 50, 70, 100]
+    for puntos in listapuntuacion:
+        while puntuacion < puntos:
+            c = rango[0]
+            a = rng.randrange(1,c)
+            b = rng.randrange(1,c)
+            res = a+b
+            res1 = int(input("¿Cuánto es " + str(a) + " + " + str(b) + "?  "))
+            if res1 == res:
+                print( "Respuesta Correcta")
+                puntuacion += 5
+                print("Puntuación: " + str(puntuacion))
+            else:
+                print( "Intentalo de nuevo" + "\n" )
+                num1 = a
+                num2 = b
+                result = a+b
+                result1 = int(input("¿Cuánto es " + str(num1) + " + " + str(num2) + "?  "))
+                if result == result1:
+                    print ("Respuesta Correcta")
                     puntuacion += 5
                     print("Puntuación: " + str(puntuacion))
                 else:
-                    print( "Intentalo de nuevo" + "\n" )
-                    num1 = a
-                    num2 = b
-                    result = a+b
-                    result1 = int(input("¿Cuánto es " + str(num1) + " + " + str(num2) + "?  "))
-                    if result == result1:
-                        texto = fuente.render("Respuesta Correcta", True, negro)
-                        pantalla.blit(texto, (350, 250))
-                        puntuacion += 5
-                        print("Puntuación: " + str(puntuacion))
-                    else:
-                        print ("Respuesta incorrecta" + "\n" + "La respuesta correcta es: " + str(result))
-            if puntuacion == puntos:
+                    print ("Respuesta incorrecta" + "\n" + "La respuesta correcta es: " + str(result))
+        if puntuacion == puntos:
+            if len(rango) != 0:
                 del rango[0]
-                print("Siguiente nivel") # esto tiene que ser un boton
-                if len(rango)== 0
-                    print("Has terminado los niveles de la suma!")
+                if puntuacion != listapuntuacion[-1]:
+                    print("Siguiente nivel") # esto tiene que ser un boton
+                else:
+                    print("Terminaste las sumas!")
+
+suma ()
         pygame.display.flip()
     pygame.quit()
 pantallaSuma()
