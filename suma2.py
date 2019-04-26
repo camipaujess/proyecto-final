f0fc0ae9680866b934e9713bba21fd01f8ec7e7d
camipaujess <49650114+camipaujess@users.noreply.github.com>
2019-04-26 16:18:49 -0500

    Update suma2.py

--- suma2.py
+++ suma2.py
@@ -1,9 +1,11 @@
 import random
 def suma():
-    puntuacion = 0
+    """Hace sumas aleatorias cada vez más díficiles con respecto a la puntuación (con cierto puntaje se vuelve más díficil)
+    """
+    puntuacion = 0 #Empieza la puntuación del juego
     rng = random.Random()
-    listapuntuacion = [50, 100, 150, 200, 250]
-    rango = [10, 30, 50, 70, 100]
+    listapuntuacion = [50, 100, 150, 200, 250] #El intervalo del puntaje en el que se cambia de nivel
+    rango = [10, 30, 50, 70, 100] #Cuando se cambia de nivel, cambian los rangos en los que se piden los números 
     for puntos in listapuntuacion:
         while puntuacion < puntos:
             c = rango[0]
@@ -12,22 +14,27 @@ def suma():
             res = a+b
             res1 = int(input("¿Cuánto es " + str(a) + " + " + str(b) + "?  "))
             if res1 == res:
+                """ Se verifica si la respuesta que dio el usuario es correcta y si lo es, suma la puntuación """
                 print( "Respuesta Correcta")
                 puntuacion += 5
                 print("Puntuación: " + str(puntuacion))
             else:
+                """ Si se responde incorrectamente, vuelve a preguntar"""
                 print( "Intentalo de nuevo" + "\n" )
                 num1 = a
                 num2 = b
                 result = a+b
                 result1 = int(input("¿Cuánto es " + str(num1) + " + " + str(num2) + "?  "))
                 if result == result1:
+                    """ En el segundo intento si contesta correctamente, imprime que fue correcto y aumenta la puntuación"""
                     print ("Respuesta Correcta")
                     puntuacion += 5
                     print("Puntuación: " + str(puntuacion))
                 else:
+                    """ Si el usuario se vuelve a equivocar, le dice cuál era la respuesta correcta"""
                     print ("Respuesta incorrecta" + "\n" + "La respuesta correcta es: " + str(result))
         if puntuacion == puntos:
+            """ Para que cambie de nivel, hacemos que los pu
             if len(rango) != 0:
                 del rango[0]
                 if puntuacion != listapuntuacion[-1]:
